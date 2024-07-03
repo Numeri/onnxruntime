d0473838be6f1a19da04938bbff95a889dd26eba
Gregor Lämmel <gregor.lammel@lilt.com>
2024-07-03 15:55:56 +0000

    scripting: fix typo

--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-docker compose exec onnxruntime_cuda python3 /test/make_graph.py
-docker compose exec onnxruntime_cude python3 /test/run_graph.py
+sudo docker compose exec onnxruntime_cuda python3 /test/make_graph.py
+sudo docker compose exec onnxruntime_cuda python3 /test/run_graph.py
